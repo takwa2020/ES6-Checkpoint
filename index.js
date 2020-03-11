b98ceb9be35adf256831d83bfeab702b0301b0e8
takwa2020 <61903442+takwa2020@users.noreply.github.com>
2020-03-11 16:43:14 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,36 +1,28 @@
-var pets = [
-  { name: "Max", type: "dog", bornOn: 2018 },
-  { name: "Angel", type: "cat", bornOn: 2015 },
-  { name: "Jasper", type: "dog", bornOn: 2016 }
+const pets = [
+  { name: 'Max', type: 'dog', bornOn: 2018 },
+  { name: 'Angel', type: 'cat', bornOn: 2015 },
+  { name: 'Jasper', type: 'dog', bornOn: 2016 }
 ];
 
 function getAge(pet) {
   return new Date().getFullYear() - pet.bornOn;
 }
 
-var petsWithAge = [];
-for (var i = 0; i < pets.length; i++) {
-  var pet = pets[i];
-  pet.age = getAge(pet);
-
-  petsWithAge.push(pet);
+const isAge = function (pet){
+	return pet.age =getAge(pet);
 }
+const petsWithAge = pets.filter(isAge);
 console.log(petsWithAge);
 
-var dogs = [];
-for (var i = 0; i < pets.length; i++) {
-  var pet = pets[i];
-  if (pet.type === "dog") {
-    dogs.push(pet);
-  }
+const isDog = function(pet){
+ 	return pet.type === 'dog';
 }
-console.log(dogs);
+const dogs = pets.filter(isDog);
+console.log(pets);
 
-var jasper;
-for (var i = 0; i < pets.length; i++) {
-  var pet = pets[i];
-  if (pet.name === "Jasper") {
-    jasper = pet;
-  }
+const hisName = function (pet){
+	return pet.name == 'Jasper';
 }
+const jasper = pets.find(hisName);
+
 console.log("Jasper is " + jasper.age + " years old");
